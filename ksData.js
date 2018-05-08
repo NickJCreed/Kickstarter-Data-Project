2b2aa56bd33645718ca9186920f52c489034945b
Luca DEttorre <ldettorre21@gmail.com>
2018-05-08 22:43:17 +0000

    Created bar chart displaying submissions per country. Added padding-left to bring y-axis into view.

--- ksData.js
+++ ksData.js
@@ -9,7 +9,7 @@ queue()
             let resultDim = ndx.dimension(dc.pluck("result"));
             let totalResults = resultDim.group()
             dc.pieChart("#resultPieChart")
-            .height(250)
+            .height(150)
             .radius(200)
             .dimension(resultDim)
             .group(totalResults)
@@ -17,10 +17,22 @@ queue()
             let popularCategoryDim = ndx.dimension(dc.pluck("subCategory"));
             let totalAmount = popularCategoryDim.group()
             dc.pieChart("#popularCategoryPieChart")
-            .height(250)
+            .height(150)
             .radius(200)
             .dimension(popularCategoryDim)
             .group(totalAmount)
+            
+            
+            let countryDim = ndx.dimension(dc.pluck("country"));
+            let totalSubPerCountry = countryDim.group()
+            dc.barChart("#submissionsBarChart")
+            .height(250)
+            .width(600)
+            .dimension(countryDim)
+            .group(totalSubPerCountry)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .xAxisLabel("Amount of submissions per country.")
             
             
             
