d2617ff1d6111ee3fb1d407c46014f1c84d04196
Luca DEttorre <ldettorre21@gmail.com>
2018-05-09 10:53:02 +0000

    Changed category pie chart to row chart for better display. added essential padding for correct value display.

--- ksData.js
+++ ksData.js
@@ -1,5 +1,5 @@
 queue()
-        .defer(d3.csv,"testBatch.csv")
+        .defer(d3.csv,"testBatchEdited.csv")
         .await(makeCharts);
         
         function makeCharts(error,transactionsData) {
@@ -9,16 +9,20 @@ queue()
             let resultDim = ndx.dimension(dc.pluck("result"));
             let totalResults = resultDim.group()
             dc.pieChart("#resultPieChart")
-            .height(150)
-            .radius(200)
+            .height(250)
+            .radius(250)
+            .innerRadius(20)
+            .externalLabels(10)
+            .externalRadiusPadding(5)
             .dimension(resultDim)
             .group(totalResults)
             
+            
             let popularCategoryDim = ndx.dimension(dc.pluck("subCategory"));
             let totalAmount = popularCategoryDim.group()
-            dc.pieChart("#popularCategoryPieChart")
-            .height(150)
-            .radius(200)
+            dc.rowChart("#popularCategoryRowChart")
+            .height(250)
+            .width(400)
             .dimension(popularCategoryDim)
             .group(totalAmount)
             
@@ -32,7 +36,19 @@ queue()
             .group(totalSubPerCountry)
             .x(d3.scale.ordinal())
             .xUnits(dc.units.ordinal)
-            .xAxisLabel("Amount of submissions per country.")
+            .xAxisLabel("Submissions per country.")
+            
+            
+            let subCatDim = ndx.dimension(dc.pluck("subCategory"));
+            let amountPledged = subCatDim.group().reduceSum(dc.pluck("pledged"));
+            dc.barChart("#categoryPledgesChart")
+            .height(300)
+            .width(450)
+            .dimension(subCatDim)
+            .group(amountPledged)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .xAxisLabel("Pledges per Category")
             
             
             
