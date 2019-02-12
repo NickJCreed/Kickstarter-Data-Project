103fb2705d9d19c2e6eada26e4e4e663e73accf3
Luca DEttorre <ldettorre21@gmail.com>
2019-02-12 23:01:16 +0000

    Changed the styling via css and editing the js file in order to display the appropriate axis lables. I also had to experiment with the margin in the js file to make sure all information in each graph was visible and not cut off.

--- ksData.js
+++ ksData.js
@@ -28,10 +28,11 @@ function makeCharts(error, transactionsData) {
     lineSpend
         .width(800)
         .height(300)
-        .margins({top:10, bottom:90, right:10, left:70})
+        .margins({top:10, bottom:60, right:20, left:70})
         .dimension(launchedDim)
         .group(total)
         .x(d3.time.scale().domain([minDate, maxDate]))
+        .xAxisLabel("September 2016 - January 2018")
         
 
 
@@ -39,11 +40,11 @@ function makeCharts(error, transactionsData) {
     let resultDim = ndx.dimension(dc.pluck("result"));
     let totalResults = resultDim.group()
     dc.pieChart("#resultPieChart")
-        .height(200)
-        .radius(200)
+        .height(300)
+        .radius(300)
         .innerRadius(10)
         .externalLabels(0)
-        .externalRadiusPadding(0)
+        .externalRadiusPadding(10)
         .dimension(resultDim)
         .group(totalResults)
         
@@ -54,6 +55,7 @@ function makeCharts(error, transactionsData) {
     dc.rowChart("#categoryRowChart")
         .height(340)
         .width(520)
+        .margins({top:10, bottom:60, right:20, left:40})
         .dimension(popularCategoryDim)
         .group(totalAmount)
         .elasticX(true)
@@ -64,11 +66,14 @@ function makeCharts(error, transactionsData) {
     dc.barChart("#submissionsBarChart")
         .height(300)
         .width(520)
+        .margins({top:10, bottom:70, right:20, left:50})
         .dimension(countryDim)
         .group(totalSubPerCountry)
         .x(d3.scale.ordinal())
+        .xAxisLabel("Countries")
         .xUnits(dc.units.ordinal)
         .elasticY(true)
+        .yAxisLabel("Submission Amount")
         
 
 
@@ -78,11 +83,14 @@ function makeCharts(error, transactionsData) {
     dc.barChart("#pledgesCategoryChart")
         .height(300)
         .width(520)
+        .margins({top:10, bottom:70, right:20, left:70})
         .dimension(subCatDim)
         .group(amountPledged)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
+        .xAxisLabel("Submission Categories")
         .elasticY(true)
+        .yAxisLabel("Amount in Dollars")
         
         
     
